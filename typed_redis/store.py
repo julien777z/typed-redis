6dfeebaac3c47f48a257a2d91f33987626f19119
Julien Kmec <me@julien.dev>
2025-08-09 19:55:04 -0700

    disable invalid name warning

--- typed_redis/store.py
+++ typed_redis/store.py
@@ -7,7 +7,7 @@ from .redis import RedisModel
 __all__ = ["Store"]
 
 
-def Store(redis_client: Redis) -> type[RedisModel]:
+def Store(redis_client: Redis) -> type[RedisModel]:  # pylint: disable=invalid-name
     """Return a base model class bound to the given Redis client."""
 
     class StoreBase(RedisModel):
